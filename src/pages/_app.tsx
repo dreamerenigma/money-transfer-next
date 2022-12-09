473f801525ff3b380c924d2accb47a7f4ccc315c
Andrey <Hitmanki@yandex.ru>
2022-12-09 13:02:37 +0400

    setup Dashboard Layout and Dashboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,8 @@ import '../styles/globals.css';
 import '@fontsource/inter';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
-import type { AppProps } from 'next/app';
+import { AppPropsWithLayout } from '../types';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 	return <Component {...pageProps} />;
 }
-
-export default MyApp;
