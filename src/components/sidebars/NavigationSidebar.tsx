2301306449bb0b64ada249e0062466e26a9faf9e
Andrey <Hitmanki@yandex.ru>
2022-12-09 19:12:14 +0400

    add navigation icons and links navigation

--- src/components/sidebars/NavigationSidebar.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './index.module.scss';
-
-export const NavigationSidebar = () => {
-	return (
-		<nav className={styles.nav}>nav sidebar</nav>
-	);
-	return <nav className={styles.nav}>nav sidebar</nav>;
-};
